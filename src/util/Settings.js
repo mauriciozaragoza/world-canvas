0d1d2b9db58060243c1a7a3589dcaf7a5d54d739
mauriciozaragoza <zaragoza.mauricio@gmail.com>
2014-10-27 23:20:11 -0600

    changed db driver to mongodb

--- src/util/Settings.js
+++ src/util/Settings.js
@@ -3,9 +3,9 @@
  * missing settings. 
  * */
 
-var fs = require("fs");
-var os = require("os");
-var jsonminify = require('jsonminify');
+ var fs = require("fs");
+ var os = require("os");
+ var jsonminify = require('jsonminify');
 
 //defaults
 exports.defaults = {
@@ -17,8 +17,8 @@ exports.defaults = {
   "dbType" : "dirty",
   //the database specific settings
   "dbSettings" : {
-                   "filename" : "var/dirty.db"
-                 }
+  	"filename" : "var/dirty.db"
+  }
 };
 
 
@@ -27,17 +27,17 @@ exports.loadSettings = function() {
 	var settings_file = "settings.json";
 	var user_settings = {};
 	try {
-		  user_settings = fs.readFileSync(settings_file).toString();
+		user_settings = fs.readFileSync(settings_file).toString();
 		  //minify to remove comments and whitepsace before parsing
 		  user_settings = JSON.parse(JSON.minify(user_settings));
-	}
-	catch(e){
-		console.error('There was an error processing your settings.json file: '+e.message);
-		process.exit(1);
-	}
-	
+		}
+		catch(e){
+			console.error('There was an error processing your settings.json file: '+e.message);
+			process.exit(1);
+		}
+		
     //copy over defaults
-	for(var k in exports.defaults){exports[k] = exports.defaults[k]}
+    for(var k in exports.defaults){exports[k] = exports.defaults[k]}
 	//go through each key in the user supplied settings and replace the defaults
 	//if a key is not in the defaults, warn the user and ignore it
 	for(var k in user_settings) {
@@ -47,12 +47,12 @@ exports.loadSettings = function() {
         }
         else
         {
-            console.warn("'Unknown Setting: '" + k + "'. This setting doesn't exist or it was removed");
+        	console.warn("'Unknown Setting: '" + k + "'. This setting doesn't exist or it was removed");
         }
-	}
+    }
     //settings specific warnings
     if(exports.dbType === "dirty"){
-        console.warn("DirtyDB is used. This is fine for testing but not recommended for production.");
+    	console.warn("DirtyDB is used. This is fine for testing but not recommended for production.");
     }
 
 };
