0907d724e0ebcc924065bd95ce178dfe2e6ee84c
Eros Espinola Gonzalez <eros.espinola.gonzalez@gmail.com>
2014-11-24 00:49:45 -0600

    merge

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
  * Module dependencies.
  */
 
-<<<<<<< HEAD
 var settings = require('./public/util/Settings.js'),
     tests = require('./public/util/tests.js'),
     draw = require('./public/util/draw.js'),
@@ -20,28 +19,6 @@ var port = settings.port;
 // Config Express to server static files from /
 app.configure(function(){
     app.use(express.static(__dirname + '/public'));
-=======
- var settings = require('./src/util/Settings.js'),
- tests = require('./src/util/tests.js'),
- draw = require('./src/util/draw.js'),
- projects = require('./src/util/projects.js'),
- db = require('./src/util/db.js'),
- express = require("express"),
- app = express(),
- paper = require('paper'),
- socket = require('socket.io'),
- async = require('async'),
- fs = require('fs');
-
-/**
- * A setting, just one
- */
- var port = settings.port;
-
-// Config Express to server static files from /
-app.configure(function(){
-	app.use(express.static(__dirname + '/'));
->>>>>>> e58436d5b49405488b81146bc675906e48f92e10
 });
 
 // Sessions
@@ -50,20 +27,12 @@ app.use(express.session({secret: 'secret', key: 'express.sid'}));
 
 // Development mode setting
 app.configure('development', function(){
-<<<<<<< HEAD
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-=======
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
->>>>>>> e58436d5b49405488b81146bc675906e48f92e10
 });
 
 // Production mode setting
 app.configure('production', function(){
-<<<<<<< HEAD
     app.use(express.errorHandler());
-=======
-	app.use(express.errorHandler());
->>>>>>> e58436d5b49405488b81146bc675906e48f92e10
 });
 
 /**
@@ -72,7 +41,6 @@ app.configure('production', function(){
 
 // Index page
 app.get('/', function(req, res){
-<<<<<<< HEAD
     res.render('index.jade', {});
 });
 
@@ -87,7 +55,17 @@ app.get('/map/', function(req, res){
     res.render('map.jade', {});
 });
 
-// Map
+// Image
+app.get('/image/:id.svg', function(req, res){
+	db.getDrawing(req.params.id, function (svg) {
+		// svg.width = 10000;
+		// svg.height = 10000;
+
+		res.send(svg);
+	});
+});
+
+// History
 app.get('/history/:id', function(req, res){
     var id = req.params.id;
     res.render('history.jade', 
@@ -101,57 +79,21 @@ app.get('/history/:id', function(req, res){
     });
 });
 
-// Map
-app.get('/top/', function(req, res){
-    res.render('top.jade', 
-    {
-        "result": [
-            {"name":"USA", "likes":"15", "date":"12 May 2014", "image":"/img/something1.png"},
-            {"name":"Mexico", "likes":"13", "date":"12 Jun 2014", "image":"/img/something2.png"},
-            {"name":"Canada", "likes":"11", "date":"12 Jul 2014", "image":"/img/something3.png"},
-            {"name":"Spain", "likes":"9", "date":"12 Jul 2014", "image":"/img/something4.png"}
-        ]
-    });
-=======
-	res.sendfile(__dirname + '/src/static/html/index.html');
-});
-
-// Drawings
-app.get('/draw/*', function(req, res){
-	res.sendfile(__dirname + '/src/static/html/draw.html');
-});
-
-// Get image
-app.get('/image/:id.svg', function(req, res){
-	db.getDrawing(req.params.id, function (svg) {
-		// svg.width = 10000;
-		// svg.height = 10000;
-
-		res.send(svg);
-	});
-});
-
-// top ranked
+// Top
 app.get('/top/:count', function(req, res){
 	db.getTopRanked(parseInt(req.params.count), function (data) {
-		res.send(data);
+		res.render('top.jade', data);
+
+		/*{
+	        "result": [
+	            {"name":"USA", "likes":"15", "date":"12 May 2014", "image":"/img/something1.png"},
+	            {"name":"Mexico", "likes":"13", "date":"12 Jun 2014", "image":"/img/something2.png"},
+	            {"name":"Canada", "likes":"11", "date":"12 Jul 2014", "image":"/img/something3.png"},
+	            {"name":"Spain", "likes":"9", "date":"12 Jul 2014", "image":"/img/something4.png"}
+	        ]
+	    }*/
+
 	});
-});
-
-// Map
-app.get('/map/*', function(req, res){
-	res.sendfile(__dirname + '/src/static/html/map.html');
-});
-
-// Map
-app.get('/history/*', function(req, res){
-	res.sendfile(__dirname + '/src/static/html/history.html');
-});
-
-// Map
-app.get('/top/*', function(req, res){
-	res.sendfile(__dirname + '/src/static/html/top.html');
->>>>>>> e58436d5b49405488b81146bc675906e48f92e10
 });
 
 /**
@@ -163,7 +105,6 @@ var io = socket.listen(server);
 
 io.sockets.setMaxListeners(0);
 
-<<<<<<< HEAD
 /**
  * Socket IO.
  */
@@ -240,61 +181,6 @@ io.sockets.on('connection', function (socket) {
         draw.addImage(room, uid, data, position, name);
         io.sockets.in(room).emit('image:add', uid, data, position, name);
     });
-
-=======
-// SOCKET IO
-io.sockets.on('connection', function (socket) {
-	socket.on('disconnect', function () {
-		console.log("Socket disconnected");
-	});
-
-	// EVENT: User stops drawing something
-	// Having room as a parameter is not good for secure rooms
-	socket.on('draw:progress', function (room, uid, co_ordinates) {
-		// console.log(room, uid, co_ordinates);
-		
-		if (!projects.projects[room] || !projects.projects[room].project) {
-			loadError(socket);
-			return;
-		}
-
-		io.in(room).emit('draw:progress', uid, co_ordinates);
-		draw.progressExternalPath(room, JSON.parse(co_ordinates), uid);
-	});
-
-	// EVENT: User stops drawing something
-	// Having room as a parameter is not good for secure rooms
-	socket.on('draw:end', function (room, uid, co_ordinates) {
-		if (!projects.projects[room] || !projects.projects[room].project) {
-			loadError(socket);
-			return;
-		}
-
-		io.in(room).emit('draw:end', uid, co_ordinates);
-		draw.endExternalPath(room, JSON.parse(co_ordinates), uid);
-	});
-
-	// User joins a room
-	socket.on('subscribe', function(data) {
-		subscribe(socket, data);
-	});
-
-	// User clears canvas
-	socket.on('canvas:clear', function(room) {
-		if (!projects.projects[room] || !projects.projects[room].project) {
-			loadError(socket);
-			return;
-		}
-		draw.clearCanvas(room);
-		io.in(room).emit('canvas:clear');
-	});
-
-	// User removes an item
-	socket.on('item:remove', function(room, uid, itemName) {
-		draw.removeItem(room, uid, itemName);
-		io.sockets.in(room).emit('item:remove', uid, itemName);
-	});
->>>>>>> e58436d5b49405488b81146bc675906e48f92e10
 });
 
 /**
@@ -302,16 +188,10 @@ io.sockets.on('connection', function (socket) {
  */
 
 function subscribe(socket, data) {
-<<<<<<< HEAD
     var room = data.room;
 
     // Subscribe the client to the room
     socket.join(room);
-
-    // If the close timer is set, cancel it
-    // if (closeTimer[room]) {
-    //  clearTimeout(closeTimer[room]);
-    // }
 
     // Create Paperjs instance for this room if it doesn't exist
     var project = projects.projects[room];
@@ -355,22 +235,3 @@ function loadFromMemory(room, socket) {
 function loadError(socket) {
     socket.emit('project:load:error');
 }
-=======
-	var room = data.room;
-
-	socket.join(room);
-
-	var project = projects.projects[room].project;
-
-	socket.emit('loading:start');
-	var value = project.exportJSON();
-	console.log("Client entered to room " + room);
-	socket.emit('project:load', { project: value });
-	socket.emit('loading:end');
-}
-
-function loadError(socket) {
-	socket.emit('project:load:error');
-}
-
->>>>>>> e58436d5b49405488b81146bc675906e48f92e10
